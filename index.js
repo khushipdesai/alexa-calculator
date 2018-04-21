e5057cc3bbcb4ef7bad1c0435027a6834a79251e
khushipdesai <khushi.desai1115@gmail.com>
2018-04-21 10:56:49 -0700

    nodejs code for Alexa calculator
    This is only the index.js file, if you want to use this you will need to create a function using the fact skill template and replace the index.js with that. You can find the templates in blueprints while creating a new function.

---
+++ index.js
@@ -0,0 +1,47 @@
+var Alexa = require('alexa-sdk');
+
+
+var handlers = {
+    
+	'LaunchRequest': function() {
+        
+		this.response.speak('Do you want to add, subtract or divide?').listen();
+
+        	this.emit(':responseReady');
+
+        
+    
+	},
+  
+	'AddIntent': function() {
+
+   	 	this.response.speak('What are the two numbers you want to add?').listen();
+
+    		this.emit(':responseReady');
+
+	},
+  
+	'SetNumIntent': function() {
+
+    		var num1 = parseInt(this.event.request.intent.slots.firstNum.value,10);
+
+    		var num2 = parseInt(this.event.request.intent.slots.secondNum.value,10);
+    
+    		var sum = num1 + num2;
+
+    		this.emit(':tell','The sum of ' + num1 + ' and ' + num2 + ' is ' + sum);
+
+	}
+
+};
+
+
+exports.handler = function(event, context, callback){
+
+    var alexa = Alexa.handler(event, context);
+
+    alexa.registerHandlers(handlers);
+
+    alexa.execute();
+
+};
